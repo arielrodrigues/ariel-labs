847ea49733e51f626d1fbce663b9e2f107c70779
arielrodrigues <arielrodrigues@protonmail.com>
2025-07-20 22:45:01 -0300

    remove .clj-kondo .cache

--- src/smart_mirror/controller.clj
+++ src/smart_mirror/controller.clj
@@ -1,11 +1,9 @@
 (ns smart-mirror.controller
   (:require [common.exceptions]
-            [common.protocols.config :as protocols.config]
             [common.protocols.gauth :as protocols.gauth]
             [smart-mirror.coordinates :as coordinates]
             [smart-mirror.http-out :as http-out]
-            [smart-mirror.time :as time])
-  (:import [com.google.auth.oauth2 UserCredentials]))
+            [smart-mirror.time :as time]))
 
 (defn weather-forecast
   [http-client]
@@ -29,9 +27,3 @@
         max-time (time/->iso-end-of-day-utc as-of)
         access-token (protocols.gauth/get-access-token token-provider config)]
     (http-out/get-gcal-events http-client access-token min-time max-time)))
-
-(comment
-  ;; @TODO:
-  ;; 1. Move config to a component (it should read an edn file)
-  ;; 2. Create tests for all functions and integration tests for the endpoint
-  )
